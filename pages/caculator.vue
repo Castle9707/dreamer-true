b3fc2d6df308c9de5a07b0c1c7e79c5e0f28afec
Castle9707 <paomincc25@gmail.com>
2024-08-27 17:25:46 +0800

    填上每個按鈕的值

--- pages/caculator.vue
+++ pages/caculator.vue
@@ -1,8 +1,11 @@
 <script setup>
-import { ref,reactive } from 'vue';
+import { ref,computed } from 'vue';
 import ButtonCal from '../components/caculator/ButtonCal.vue';
 
-const number = reactive([0,1,2,3,4,5,6,7,8,9])
+const currentNumber = ref('')
+const previousNumber = ref('')
+const operation = ref('')
+const result = ref('')
 </script>
 
 <template>
@@ -11,36 +14,36 @@ const number = reactive([0,1,2,3,4,5,6,7,8,9])
   </div>
   <div class="rounded-2xl w-72 h-fit bg-gradient-to-r from-gray-400 to-gray-200 inner-shadow">
     <div class="result w-auto h-24 text-2xl text-white rounded-t-2xl flex justify-end items-end bg-gradient-to-r from-gray-900 to-gray-500">
-      <h4 class="mx-5 mb-2">result</h4>
+      <h4 class="mx-5 mb-2">{{ displayValue }}</h4>
     </div>
     <div class="flex mt-2 mx-3">
-      <ButtonCal class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600" />
-      <ButtonCal class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600" />
-      <ButtonCal class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600" />
-      <ButtonCal class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400" />
+      <ButtonCal value="C" class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600 text-white" />
+      <ButtonCal value="±" class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600 text-white" />
+      <ButtonCal value="%" class="from-gray-900 to-gray-500 active:from-gray-950 active:to-gray-600 text-white" />
+      <ButtonCal value="÷" class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400 text-white" />
     </div>
     <div class="flex mx-3">
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400" />
+      <ButtonCal value="7" />
+      <ButtonCal value="8" />
+      <ButtonCal value="9" />
+      <ButtonCal value="x" class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400 text-white" />
     </div>
     <div class="flex mx-3">
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400" />
+      <ButtonCal value="4" />
+      <ButtonCal value="5" />
+      <ButtonCal value="6" />
+      <ButtonCal value="-" class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400 text-white" />
     </div>
     <div class="flex mx-3">
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal />
-      <ButtonCal class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400" />
+      <ButtonCal value="1" />
+      <ButtonCal value="2" />
+      <ButtonCal value="3" />
+      <ButtonCal value="+" class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400 text-white" />
     </div>
     <div class="flex mx-3">
-      <ButtonCal class="w-30 mb-5" />
-      <ButtonCal />
-      <ButtonCal class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400" />
+      <ButtonCal value="0" class="w-30 mb-5" />
+      <ButtonCal value="." />
+      <ButtonCal value="=" class="from-orange-600 to-orange-300 active:from-orange-700 active:to-orange-400 text-white" />
     </div>
   </div>
   
