d05ef5466e977f87955038e2a7bbb89d7e8838d9
Castle9707 <paomincc25@gmail.com>
2024-09-03 17:22:39 +0800

    基本路徑變更測試

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
   },
 
   router: {
-    base: process.env.NODE_ENV === "production" ? "/dreamer-true/" : "/",
+    base: "/dreamer-true/",
+    // base: process.env.NODE_ENV === "production" ? "/dreamer-true/" : "/",
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -46,4 +47,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  compatibilityDate: "2024-09-03",
 };
