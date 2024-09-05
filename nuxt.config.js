6fd4d1d621a8825910309f336e81b3baa34f6d77
Castle9707 <paomincc25@gmail.com>
2024-09-05 10:51:34 +0800

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,22 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
+  // 專案名稱
+  target: "static",
+  // 全域頁面轉場動畫
+  transition: {
+    name: "page",
+    mode: "out-in",
+  },
+  // 路由配置
   router: {
     base: "/dreamer-true/",
     // base: process.env.NODE_ENV === "production" ? "/dreamer-true/" : "/",
+  },
+
+  // 產生靜態頁面配置
+  generate: {
+    fallback: true, // 生成404.html，用於GitHub Pages部署
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
