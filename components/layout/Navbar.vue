6d2918befc2b8e260cb8562c6a720bf040f5b983
Castle9707 <paomincc25@gmail.com>
2024-09-12 17:29:19 +0800

    首頁連結修正

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -11,10 +11,7 @@ const toggleMenu = () => {
 <template>
 <nav class="block w-full max-w-screen-lg px-4 py-2 mx-auto text-white bg-white shadow-md rounded-md lg:px-8 lg:py-3 mt-10">
   <div class="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
-    <a href="/"
-      class="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">
-      logo00o
-    </a>
+    <NuxtLink to="/" class="mr-4 block cursor-pointer py-1.5 text-base text-slate-800 font-semibold">logo00o</NuxtLink>
     <div class="hidden lg:block">
       <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
         <li
