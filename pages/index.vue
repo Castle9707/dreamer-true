7992bfb9b24fb3af74628b163c48765b133f8044
Castle9707 <paomincc25@gmail.com>
2024-09-13 14:13:27 +0800

    首頁

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
-<template>
-  <Tutorial/>
-  <div>12345</div>
-</template>
-
 <script>
+import AlbumCard from '../components/card/AlbumCard.vue'
+
 export default {
-  name: 'IndexPage'
+    name: 'IndexPage',
+    components: { AlbumCard }
 }
 </script>
+
+<template>
+  <div>我是首頁。</div>
+  <!-- 準備先排版一下卡片 4張*2 -->
+  <AlbumCard />
+</template>
