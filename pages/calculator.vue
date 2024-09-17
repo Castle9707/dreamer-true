de01a6fa5beca29dcd3c61660804066e17b9f52e
Castle9707 <paomincc25@gmail.com>
2024-09-17 14:36:46 +0800

    計算機樣式微調

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -388,8 +388,8 @@ const displayValue = computed(() => {
   <div>
     Page: Calculator
   </div>
-  <div class="rounded-2xl w-72 h-fit mx-auto bg-gradient-to-r from-gray-400 to-gray-200 cal-shadow">
-    <div class="result w-auto h-24 text-2xl text-white rounded-t-2xl flex justify-end items-end bg-gradient-to-r from-gray-900 to-gray-500">
+  <div class="rounded-2xl w-72 h-fit mx-auto mt-10 bg-gradient-to-br from-gray-300 to-gray-200 cal-shadow">
+    <div class="result w-auto h-24 text-2xl text-white rounded-t-2xl flex justify-end items-end bg-gradient-to-br from-gray-900 to-gray-500">
       <h4 class="mx-5 mb-2">{{ displayValue }}</h4>
     </div>
     <div class="flex mt-2 mx-4">
@@ -426,8 +426,9 @@ const displayValue = computed(() => {
 
 <style>
 .cal-shadow {
-  box-shadow: inset 0 0 15px 8px rgb(0 0 0 / 0.25),
-  inset 0 0 3px 1px rgb(255 255 255 / 1),
-  0px 10px 20px 20px rgb(80 80 82 / 0.15);
+  box-shadow: inset 0 0 3px 1px rgb(255 255 255 / 1),
+  inset 0 0 8px 4px rgb(0 0 0 / 0.2),
+  0px 10px 20px -20px rgb(80 80 82 / 0.15),
+  20px 34px 75px 0px rgb(0 0 0 / 0.25);
 }
 </style>
