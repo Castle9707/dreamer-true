e556bcabc2f71f9f28e52b506525719a29fe0593
Castle9707 <paomincc25@gmail.com>
2024-09-30 12:58:54 +0800

    加回@nuxt/image

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default defineNuxtConfig({
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ["@pinia/nuxt"],
+  modules: ["@nuxt/image", "@pinia/nuxt"],
 
   image: {
     dir: "static/images",
