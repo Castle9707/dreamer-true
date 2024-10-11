1a5ae1ae19baae864ce400cb97d4b6d276b3596a
Castle9707 <paomincc25@gmail.com>
2024-10-11 10:08:49 +0800

    鼠標滑動卡片

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<script setup>
+import InclineCard from '@/components/contact/InclineCard.vue'
+</script>
+
+<template>
+  <div class="w-full h-24 bg-white my-30 relative">1</div>
+    <InclineCard 
+      :originalImage="'hiperson.jpg'"
+      :duotoneImage="'wannunhotel.jpg'" 
+    />
+  
+</template>
